6ae60ec51afcebeb0a4b865779bef71b189f62b3
parkermuir <parker.muir@gmail.com>
2018-06-20 14:17:26 -0400

    Completed major diagonal function
    - Refactored to use negative indexes

--- src/Board.js
+++ src/Board.js
@@ -82,7 +82,6 @@
       var conflict = this.rows()[rowIndex].reduce(function (a, v) {
         return a += v;
       }, 0);
-      console.log(conflict);
       return conflict > 1;
     },
 
@@ -128,13 +127,33 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      
-      return false; // fixme
+      var diagonal = [];
+      var y = majorDiagonalColumnIndexAtFirstRow;
+
+      for (var x = 0; (x < this.rows().length && y < this.rows().length); x++) {
+        if (y < 0) {
+          y++;
+          continue;
+        } else {
+          diagonal.push(this.rows()[x][y]);
+          y++;
+        }
+       
+      }
+
+      return (diagonal.reduce(function(a, v) {
+        return a += v;
+      }, 0) > 1);
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      for (var y = ((-this.rows().length) + 1); y < this.rows().length; y++) {
+        if (this.hasMajorDiagonalConflictAt(y) === true) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -144,12 +163,36 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var diagonal = [];
+      var y = minorDiagonalColumnIndexAtFirstRow;
+      var decrementY = false;
+
+      //iterate
+
+      for (var x = 0; (x < this.rows().length && y >= 0); x++) {
+        var rowTotal = this.rows()[x].reduce((a, v) => a + v);
+        if (rowTotal > 0) {
+          decrementY = true;
+        }
+
+        diagonal.push(this.rows()[x][y]);
+        if (decrementY) {
+          y--;
+        }
+      }
+
+      return (diagonal.reduce((a, v) => a + v) > 1);
+
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      for (var y = 0; y < this.rows().length; y++) {
+        if (this.hasMinorDiagonalConflictAt(y) === true) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
